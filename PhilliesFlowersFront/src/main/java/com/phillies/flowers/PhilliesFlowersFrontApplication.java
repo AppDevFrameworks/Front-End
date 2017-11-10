c5695c711d66a5bb805ab025cc1ec67df5e7995c
Robert Crowley <robert.crowley1995@gmail.com>
2017-11-10 11:37:34 +0000

    Created Project
    10th Novement

---
+++ PhilliesFlowersFront/src/main/java/com/phillies/flowers/PhilliesFlowersFrontApplication.java
@@ -0,0 +1,12 @@
+package com.phillies.flowers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilliesFlowersFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilliesFlowersFrontApplication.class, args);
+	}
+}
